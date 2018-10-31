3766ad5e19a81d2d76f9a87cdbb353bb216ddb69
nBow <nbow51@gmail.com>
2018-10-30 21:36:02 -0700

    dank update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,14 @@
-
 $.fn.dankMeme = function () {
     // Create functions inside this plugin to avoid global variables/collisions.
     var t = this;
     t.colorsArr = ["peach", "purple", "blue", "aqua", "amy-crisp", "ripe-malinka", "morpheus-den", "dusty-grass", "tempting-azure"];
+    
     t.memeCall = function() {
-        t.input = $("#input-word").val().trim();
+        t.getInput();
         var settings = {
             "async": true,
             "crossDomain": true,
-            "url": "https://api.imgur.com/3/gallery/search/viral/year/1/?q=" + t.input + "+memes",
+            "url": "https://api.imgur.com/3/gallery/search/viral/year/1/?q=" + t.input,
             "method": "GET",
             "headers": {
                 "Authorization": "Client-ID be876a4140064ba"
@@ -18,47 +18,87 @@ $.fn.dankMeme = function () {
         $.ajax(settings).done(function (response) {
             t.results = response.data;
             console.log(t.results);
-            t.colorPick = t.colorsArr[Math.floor(Math.random() * 9)]
-            t.rand =  t.results[Math.floor(Math.random() * t.results.length)]
-            t.memeDiv = $("#meme-holder");
-            t.memeDiv.html("");
-            t.div = $("<div>");
-            t.img = $("<img>");
-            t.p = $("<p>")
+            t.colorPick = t.colorsArr[Math.floor(Math.random() * 9)];
+            t.rand =  t.results[Math.floor(Math.random() * t.results.length)];
+            var c = t.containers();;
             if (t.results.length > 0) {
-                t.div.attr({
+                c.div.attr({
                     "width": "300px",
                     "height": "auto"
                 });
-                t.p.text(t.rand.title).addClass(t.colorPick + "-gradient").css({
+                c.p.text(t.rand.title).addClass(t.colorPick + "-gradient z-depth-1").css({
                     "text-align": "center",
                     "color": "white",
                     "font-family": "Poor Story"
                 });
-                t.img.attr({
+                c.img.attr({
                     "src": t.rand.images[0].link,
                     "class": "img-fluid"
-                });
-                t.div.append(t.p, t.img);
-                t.memeDiv.append(t.div);
+                }).addClass("z-depth-1");
+                c.div.append(c.p, c.img);
+                c.memeDiv.append(c.div);
             } else {
-                t.p.text("No memes here").addClass(t.colorPick + "-gradient").css({
+                c.p.text("No memes here").addClass(t.colorPick + "-gradient z-depth-1").css({
                     "text-align": "center",
                     "color": "white",
                     "font-family": "Poor Story"
                 });
-                t.img.attr({
+                c.img.attr({
                     "src": "assets/images/tenor.png",
                     "class": "img-fluid"
-                });
-                t.memeDiv.append(t.p, t.img);
+                }).addClass("z-depth-1");
+                c.memeDiv.append(c.p, c.img);
             };
         });
     }
 
     t.urbanCall = function(){
+        t.getInput();
+        var settings = {
+            "url": "http://api.urbandictionary.com/v0/define?term=" + t.input,
+            "method": "GET"
+        };
 
+        $.ajax(settings).done(function (response) {
+            t.results = response.list;
+            console.log(t.results);
+            t.rand = t.results[Math.floor(Math.random() * t.results.length)];
+            console.log(t.rand);
+            var c = t.containers();
+            c.p.text("[URBAN] " + t.rand.definition).css({
+                "padding-left": "10px",
+                "color": "white",
+                "font-family": "Poor Story"
+            }).addClass("lady-lips-gradient z-depth-1");
+            c.head.text(t.rand.word + " by: " + t.rand.author).css({
+                "padding-left": "5px",
+                "color": "white",
+                "font-family": "Poor Story"
+            }).addClass("rainy-ashville-gradient z-depth-1");
+            c.urbanDiv.append(c.head, c.p);
+
+        })
     }
+    t.cleanUp = function(){
+        var c = t.containers();
+        $("#input-word").val("");
+        c.urbanDiv.html("");
+        c.memeDiv.html("");
+    }
+    t.containers = function(){
+        var c = this;
+        c.div = $("<div>");
+        c.img = $("<img>");
+        c.p = $("<p>");
+        c.head = $("<h3>");
+        c.memeDiv = $("#meme-holder");
+        c.urbanDiv = $("#urban");
+        return c;
+    }
+    t.getInput = function(){
+        t.input = $("#input-word").val().trim();
+    }
+    
     return t;
 };
 $(document).ready(function () {
@@ -67,6 +107,9 @@ $(document).ready(function () {
     $("body").on("click", "#submit", function (event) {
         event.preventDefault();
         dank.memeCall();
+        dank.urbanCall();
+        dank.cleanUp();
+
     })
 });
 
